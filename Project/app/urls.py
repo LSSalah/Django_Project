7c9575efe29658e1c6ed5c917812e470572535a5
Salah <largers2013@gmail.com>
2020-12-26 21:42:13 +0200

    Final updates

--- Project/app/urls.py
+++ Project/app/urls.py
@@ -12,8 +12,11 @@ urlpatterns = [
     path('userorder',views.userorder),
     path('driverorder',views.driverorder),
     path('editacc/<user_id>', views.edit_account),
+    path('editaccdriver/<driver_id>', views.edit_accountdriver),
     path('update/<user_id>', views.submit),
+    path('updatedriver/<driver_id>', views.submitdriver),
     path('remove/<order_id>', views.removeorder) ,
+    path('done/<order_id>', views.doneorder) ,
     path('order', views.post_order),
     path('order/<order_id>', views.addorder),
 ]
--- Project/app/views.py
+++ Project/app/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect , HttpResponse
 from django.contrib import messages
 import bcrypt
 from .models import *
 
 def about(request):
     try:
-        if request.session:
+        if "user_id" in request.session:
             current_user = User.objects.get(id = request.session['user_id'])
+            context = {
+                "user": current_user,
+                    
+                    }
+            return render(request, 'about.html',context )
+        else:
             current_driver = Driver.objects.get(id = request.session['driver_id'])
             context = {
-            "user": current_user,
             "driver" : current_driver
-                
+            
                 }
             return render(request, 'about.html',context )
     except:
@@ -19,29 +24,39 @@ def about(request):
 
 def drivers(request):
     try:
-        if request.session:
+        if "user_id" in request.session:
             current_user = User.objects.get(id = request.session['user_id'])
+            context = {
+                "user": current_user,
+                    
+                    }
+            return render(request, 'drivers.html',context )
+        else:
             current_driver = Driver.objects.get(id = request.session['driver_id'])
             context = {
-            "user": current_user,
             "driver" : current_driver
-                
+            
                 }
-        return render(request, 'drivers.html',context )
+            return render(request, 'drivers.html',context )
     except:
         return render(request, 'drivers.html')
 
 def contact(request):
     try:
-        if request.session:
+        if "user_id" in request.session:
             current_user = User.objects.get(id = request.session['user_id'])
+            context = {
+                "user": current_user,
+                    
+                    }
+            return render(request, 'contact.html',context )
+        else:
             current_driver = Driver.objects.get(id = request.session['driver_id'])
             context = {
-            "user": current_user,
             "driver" : current_driver
-                
+            
                 }
-        return render(request, 'contact.html',context )
+            return render(request, 'contact.html',context )
     except:
         return render(request, 'contact.html' )
 
@@ -89,7 +104,7 @@ def registerdriver(request):
             phone = request.POST["phone"]
             address = request.POST["address"]
             password = request.POST["password"]
-            car = request.POST["car"]
+            # car = request.POST["car"]
             password = bcrypt.hashpw(request.POST['password'].encode(),bcrypt.gensalt()).decode()
             this_user = Driver.objects.create(first_name = first_name,last_name = last_name, email = email, password = password, phone = phone , address = address , birthday = birthday , car = car)
             request.session['driver_id'] = this_user.id
@@ -112,8 +127,6 @@ def register(request):
 
 def login(request):
     try:
-        print ('-' * 30 )
-        print ('*' * 30 )
         if request.POST["option"] == "user":
             this_user = User.objects.get(email=request.POST['email'])
             print(this_user.password)
@@ -127,7 +140,6 @@ def login(request):
                 return redirect('/')
         if request.POST["option"] == "driver":
             driver = Driver.objects.get(email=request.POST['email'])
-            print ('+' * 30 )
             if bcrypt.checkpw(request.POST['password'].encode(), driver.password.encode()):
                 request.session['driver_id'] = driver.id
                 messages.error(request, "Successfully registered (or logged in)!")
@@ -140,46 +152,48 @@ def login(request):
         return redirect('/')
 
 def logout(request):
-    del request.session['user_id']
+    request.session.clear()
     messages.error(request, "You have successfully logged out")
     return redirect('/')
 
+
 def userorder(request):
-    try:
+    if "user_id" in request.session:
         current_user = User.objects.get(id = request.session['user_id'])
-        current_driver = Driver.objects.get(id = request.session['driver_id'])
         myorders = Order.objects.filter(poster=current_user)
         context = {
-        "user": current_user ,
-        "myorders" : myorders,
-        "driver" : current_driver
-            }
+            "user": current_user ,
+            "myorders" : myorders,
+                }
         return render(request, 'userorder.html',context )
-    except:
-        return render(request,"userorder.html")
-
+    else:
+        return HttpResponse("<h2>You are not Allowed to View this page</h2>")
 def driverorder(request):
-    try:
-        current_user = Driver.objects.get(id = request.session['user_id'])
-        favourites = Order.objects.filter(deliver=current_user)
+    if "driver_id" in request.session:
+        current_driver = Driver.objects.get(id = request.session['driver_id'])
+        favourites = Order.objects.filter(deliver=current_driver)
         context = {
-        "user": current_user,
-        'order_list' : Order.objects.all(),
-        "favourites" : favourites
-            }
+            "user": current_driver,
+            'order_list' : Order.objects.all(),
+            "favourites" : favourites
+                }
         return render(request, 'driverorder.html',context )
-    except:
-        return render(request,"driverorder.html")
+    else:
+        return HttpResponse("<h2>You are not Allowed to View this page</h2>")
+
 
 def edit_account(request, user_id):
-    if request.session['user_id']:
+    if "user_id" in request.session:
         context = {
         "user": User.objects.get(id=request.session['user_id']),
         }
-        return render(request, 'account.html', context)
-    if request.session['driver_id']:
+        return render(request, "account.html", context)
+    else:
+        return redirect('/')
+def edit_accountdriver(request, driver_id):
+    if "driver_id" in request.session:
         context = {
-        "user": Driver.objects.get(id=request.session['driver_id']),
+        "driver": Driver.objects.get(id=request.session['driver_id']),
         }
         return render(request, 'account.html', context)
     else:
@@ -203,6 +217,23 @@ def submit(request, user_id):
         messages.success(request, "Account successfully updated")
         return redirect('/editacc/<user_id>')
 
+def submitdriver(request, driver_id):    
+    selected = Driver.objects.get(id= request.session['driver_id'])
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/editaccdriver/{driver_id}')
+    else:
+        password = bcrypt.hashpw(request.POST['password'].encode(),bcrypt.gensalt()).decode()
+        selected.first_name = request.POST['first_name']
+        selected.last_name = request.POST['last_name']
+        selected.email = request.POST['email']
+        selected.password = password
+        selected.save()
+        messages.success(request, "Account successfully updated")
+        return redirect(f'/editaccdriver/{driver_id}')
+
 def removeorder(request, order_id):
     if 'user_id' not in request.session:
         messages.error(request, "Must be logged in!")
@@ -216,10 +247,20 @@ def removeorder(request, order_id):
     return redirect('/userorder')
 
 def addorder(request, order_id):
-    fav = Driver.objects.get(id = request.session['driver_id'])
+    fav = Driver.objects.get(id = request.session['user_id'])
     Order.objects.get(id = order_id).deliver.add(fav)
     return redirect('/driverorder')
 
+def doneorder(request, order_id):
+    if 'driver_id' not in request.session:
+        messages.error(request, "Must be logged in!")
+        return redirect('/')
+
+    current_user = Driver.objects.get(id=request.session['driver_id'])
+    current_Order = Order.objects.get(id = order_id).deliver
+    current_Order.delete()
+
+    return redirect('/driverorder')
 
 def post_order(request):
     if request.method == 'POST':
